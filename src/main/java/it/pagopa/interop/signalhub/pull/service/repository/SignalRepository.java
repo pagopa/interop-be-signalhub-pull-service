8226ffbfd906703baa6d50e0a875375a35c1c5f6
DIR\noemi.luongo <noemi.luongo@accenture.com>
2023-09-27 10:21:18 +0200

    fix

--- src/main/java/it/pagopa/interop/signalhub/pull/service/repository/SignalRepository.java
+++ src/main/java/it/pagopa/interop/signalhub/pull/service/repository/SignalRepository.java
@@ -1,6 +1,6 @@
 package it.pagopa.interop.signalhub.pull.service.repository;
 
-import it.pagopa.interop.signalhub.pull.service.rest.v1.dto.Signal;
+import it.pagopa.interop.signalhub.pull.service.entities.Signal;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +10,7 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface SignalRepository extends ReactiveCrudRepository<Signal, Long> {
 
-    @Query("select * from signal s where s.eservice_id= :eserviceId and s.signal_id BETWEEN indexSignal AND indexSignal " +
+    @Query("select * from signal s where s.eservice_id= :eserviceId and s.signal_id BETWEEN indexSignalStart AND indexSignalEnd " +
             "order by s.signal_id;")
     Mono<Flux<Signal>> findSignal(String eserviceId, Long indexSignalStart, Long indexSignalEnd);
 
