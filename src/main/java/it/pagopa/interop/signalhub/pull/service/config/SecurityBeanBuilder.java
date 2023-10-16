8e6dfbd55e98979ed80e4e84e41960a822cf1e23
DIR\noemi.luongo <noemi.luongo@accenture.com>
2023-10-16 18:10:52 +0200

    minimal fix

--- src/main/java/it/pagopa/interop/signalhub/pull/service/config/SecurityBeanBuilder.java
+++ src/main/java/it/pagopa/interop/signalhub/pull/service/config/SecurityBeanBuilder.java
@@ -2,15 +2,19 @@ package it.pagopa.interop.signalhub.pull.service.config;
 
 import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.JwkProviderBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityBeanBuilder {
 
+    @Value("${jwk.provider.endpoint}")
+    private String jwkProviderEndpoint;
+
     @Bean
     public JwkProvider jwkProvider(){
-        return new JwkProviderBuilder("https://uat.interop.pagopa.it")
+        return new JwkProviderBuilder(jwkProviderEndpoint)
                 .build();
     }
 
