dbedc43d2fd486e00967ff63226cff80233895be
cristopher.moccia <cristopher.moccia@accenture.com>
2023-11-29 15:53:49 +0100

    resolved reports

--- src/main/java/it/pagopa/interop/signalhub/pull/service/SignalHubPullServiceApplication.java
+++ src/main/java/it/pagopa/interop/signalhub/pull/service/SignalHubPullServiceApplication.java
@@ -11,9 +11,6 @@ import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 public class SignalHubPullServiceApplication {
 
 	public static void main(String[] args) {
-
-		log.error("AVAILABLE PROCESSOR: {}", Runtime.getRuntime().availableProcessors() );
-
 		SpringApplication.run(SignalHubPullServiceApplication.class, args);
 	}
 
